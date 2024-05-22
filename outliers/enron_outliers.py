a634f86fc60c09e4b6709de623135e19deeb6935
Jinay <avengergroot046@gmail.com>
2024-05-22 15:56:39 +0530

    yes

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,17 +1,24 @@
 import joblib
 import sys
 import matplotlib.pyplot
-sys.path.append("../tools/")
+sys.path.append(r"C:\Users\dell\OneDrive\Documents\VS Codes\Udacity-UD120-Migrated-to-Python-3\tools")
 from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = joblib.load( open("../final_project/final_project_dataset.pkl", "rb") )
+data_dict = joblib.load( open(r"C:\Users\dell\OneDrive\Documents\VS Codes\Udacity-UD120-Migrated-to-Python-3\final_project\final_project_dataset.pkl", "rb") )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter(salary,bonus)
 
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
